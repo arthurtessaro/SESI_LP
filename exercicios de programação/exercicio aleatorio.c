1781bec8d515b6875fb7698dda5721566b2fedf0
arthur <arthurfrassontessaro@gmail.com>
2025-08-07 11:15:10 -0300

    exercicio entrada de dados

---
+++ exercicios de programação/exercicio aleatorio.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+   void main(){
+    int a = 34;
+    float b = 2.37;
+    char c ='A';
+    char nome [6] = "ARTHUR";
+    
+    printf("valor de a: %d", a);
+    printf("\n valor de b: %2.f", b);
+    printf("\n valor de c: %c", c);
+    printf("\n valor de nome: %s", nome);
+    
+    printf("\n valor p1: %c", nome [0]);
+    printf("\n valor p1: %c", nome [1]);
+    printf("\n valor p1: %c", nome [2]);
+    printf("\n valor p1: %c", nome [3]);
+    printf("\n valor p1: %c", nome [4]);
+    printf("\n valor p1: %c", nome [5]);
+   }
---
+++ exercicios de programação/exercicio_01.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+ void main(){
+     char NOME[255] = "";
+     printf("digite seu nome: ");
+     scanf("%s", NOME);
+  printf("olá, %s!  Seja bem-vindo", NOME);
+     
+ }
